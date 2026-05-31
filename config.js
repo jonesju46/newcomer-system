35a7627cef3253a10d6fed15f1323cf04156700c
jonesju46 <128109180+jonesju46@users.noreply.github.com>
2026-06-01 00:19:16 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CHURCH_SHEETS_CONFIG = {
-  webAppUrl: "https://script.google.com/macros/s/AKfycbyCn9mXOI-rMOjmJpA2mivOR1EMmxTHxeQbjPb0NvJBT0j6Z7ecys-fUfI9MMkCTNbm/exec"
+  webAppUrl: "https://script.google.com/macros/s/AKfycbxyqvzDv9gvE7DOAYBEIoDm7U26raJmxQdag8XJH6guCMyzvchKhabUfeXglMUD1kWF/exec"
 };
